baeb14f4d0fdd70ffd3069e6023057f915968da7
haojiongwang <haojiongwang@gmail.com>
2016-09-09 20:15:19 -0400

    Python Script

---
+++ a1.py
@@ -0,0 +1,119 @@
+import glob  
+import re
+
+
+headStr1 = 'writes'
+headStr2 = 'wrote'
+headStr3 = 'said'
+headStr4 = 'Subject : Re : '
+headStr5 = 'Subject : '
+
+regex = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
+                    "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
+                    "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
+
+instant = []
+
+path = '/Users/haojiongwang/Desktop/CORNELL/cs4740/data_corrected/classification task/motorcycles/train_docs/*.txt'   
+files=glob.glob(path)   
+for file in files:     
+    f=open(file, 'r') 
+    line = f.read().replace('\n', '')  
+    if line.find(headStr1) != -1:
+        ind = line.find(headStr1)
+        data = line[(ind+len(headStr1)):]
+        
+    elif line.find(headStr2) != -1:
+        ind = line.find(headStr2)
+        data = line[(ind+len(headStr2)):]
+        
+    elif line.find(headStr3) != -1:
+        ind = line.find(headStr3)
+        data = line[(ind + len(headStr3)):]
+        
+    elif line.find(headStr4) != -1:
+        ind = line.find(headStr4)
+        data = line[(ind + len(headStr4)):]
+        
+    elif line.find(headStr5) != -1:
+        ind = line.find(headStr5)
+        data = line[(ind + len(headStr5)):]
+    else:
+        data = line
+    
+     
+    data_clean = re.sub('[>]', '', data)
+    #print file, '\n', data_clean, '\n'
+    #pattern = re.match('-.+', data_clean)
+    #data_clean = re.sub('[\|:)()#]', '', data_clean)
+
+    idx = data_clean.find('- - -')
+
+    
+    
+    if (data_clean.find('- - -') != -1 and len(data_clean[idx+1:]) <150):
+        data_c1 = data_clean[0:idx+1]
+        #print file, '\n','a', '\n'
+    else:
+        data_c1 = data_clean
+    #print file, '\n', data_c1, '\n'
+
+    #print file, '\n', data_c1, '\n'
+
+    
+    
+
+    
+    idx2 = data_c1.find('- -')
+
+    if (data_c1.find('- -') != -1 and len(data_c1[idx2+1:]) <150):
+        data_c2 = data_c1[0:idx2+1]
+    else:
+        data_c2 = data_c1
+
+    print file, '\n', data_c2, '\n'
+
+
+    '''
+    idx = data_clean.find('= = =')
+
+    if '= = =' in data_clean & len(data_clean[idx:]) < 150:
+        print file, '\n', data_clean[idx:], '\n'
+
+
+
+         
+
+    #print file, '\n', data_clean, '\n'
+    
+
+    rmList = '> " | # : - ) ( * : [ ] '
+    rmList = rmList.split()
+    for n in rmList:
+        data_clean= data.replace(n, '')
+
+    
+    
+    for email in re.findall(regex, data_clean):
+        #data_clean = re.sub(email[0],'', data_clean)
+        idx = data_clean.rfind(email[0])
+        if len(data_clean[idx:] <150:
+            data_clean 
+
+            print file, '\n', data_clean[idx:], '\n'
+
+    '''
+
+
+
+    
+    data_after = ' '.join(data_clean.split())
+
+    #print file, '\n', data_clean, '\n'
+
+      
+       
+    
+    
+    
+    
