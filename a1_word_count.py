c1306b535aa2a3939162d3a4a4dfb0d0a3558744
WeinanHuang <wh377@cornell.edu>
2016-09-10 20:22:20 -0400

    Sat 8:21 version

--- a1_word_count.py
+++ a1_word_count.py
@@ -3,53 +3,65 @@ import glob
 import os
 import re
 import collections
+#import pandas as pd
 
-
-headStr1 = 'writes'
-headStr2 = 'wrote'
-headStr3 = 'said'
+#first set up some string to cut off the head of the e-mail
+headStr1 = 'writes :'
+headStr2 = 'wrote :'
+headStr3 = 'said :'
 headStr4 = 'Subject : Re : '
 headStr5 = 'Subject : '
+
+#this regular expression is set to capture the email address
 regex = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`"
                     "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|"
                     "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
 
+#this is to set a regular expression which will be used to capture the first occurance of letter
+r_head = re.compile("([a-zA-Z]+?)")
+
 Text = ''
 
-path = '/Users/Raymond/Downloads/data_corrected/classification task/atheism/train_docs/*.txt'
+path = '/Users/Raymond/Downloads/data_corrected/classification task/autos/train_docs/*.txt'
+
+#read all the file now
 files=glob.glob(path)
 for file in files:
     f=open(file, 'r')
-    line = f.read().replace('\n', '').lower()
 
+    line = f.read().replace('\n', '').lower()
     # leave out head (Subject, Email Address, etc)
-    if line.find(headStr1) != -1:
-        ind = line.find(headStr1)
+
+    if line.rfind(headStr1) != -1:
+        ind = line.rfind(headStr1)
         data = line[(ind+len(headStr1)):]
 
-    elif line.find(headStr2) != -1:
-        ind = line.find(headStr2)
+    elif line.rfind(headStr2) != -1:
+        ind = line.rfind(headStr2)
         data = line[(ind+len(headStr2)):]
 
-    elif line.find(headStr3) != -1:
-        ind = line.find(headStr3)
+    elif line.rfind(headStr3) != -1:
+        ind = line.rfind(headStr3)
         data = line[(ind + len(headStr3)):]
 
-    elif line.find(headStr4) != -1:
-        ind = line.find(headStr4)
+    elif line.rfind(headStr4) != -1:
+        ind = line.rfind(headStr4)
         data = line[(ind + len(headStr4)):]
 
-    elif line.find(headStr5) != -1:
-        ind = line.find(headStr5)
+    elif line.rfind(headStr5) != -1:
+        ind = line.rfind(headStr5)
         data = line[(ind + len(headStr5)):]
+
     else:
         data = line
 
-
+    #clean the symbol >
     data_clean = re.sub('[>]', '', data)
     #print file, '\n', data_clean, '\n'
     #pattern = re.match('-.+', data_clean)
     #data_clean = re.sub('[\|:)()#]', '', data_clean)
+
+    #this is to clean the signture after - - -
 
     idx = data_clean.find('- - -')
 
@@ -60,14 +72,8 @@ for file in files:
         #print file, '\n','a', '\n'
     else:
         data_c1 = data_clean
-    #print file, '\n', data_c1, '\n'
-
-    #print file, '\n', data_c1, '\n'
-
-
-
-
-
+    
+    #this is to clean the signture after - - 
     idx2 = data_c1.find('- -')
 
     if (data_c1.find('- -') != -1 and len(data_c1[idx2+1:]) <150):
@@ -89,30 +95,66 @@ for file in files:
 
 
     #print file, '\n', data_clean, '\n'
-
-
-    for email in re.findall(regex, data_clean):
-        #data_clean = re.sub(email[0],'', data_clean)
-        idx = data_clean.rfind(email[0])
-        if len(data_clean[idx:] <150:
-            data_clean
-
-            print file, '\n', data_clean[idx:], '\n'
-
     '''
 
+    #delete all the email address
+    for email in re.findall(regex, data_clean):
+        data_clean = re.sub(email[0],'', data_clean)
+
+
+    #replace all the " ' " to space   
+    data_clean = data_clean.replace(" ' ",'')
+    #print file, '\n', data_clean, '\n'
+    
+
     # replace uneccesary notation
-    rmList = '> " | # : - ) ( * : [ ] } {'
+    rmList = '> " | # : - ) ( *  [ ] } {+ = ^_'
     rmList = rmList.split()
     for n in rmList:
-        data = data.replace(n, '')
+        data_clean = data_clean.replace(n, '')
 
     # switch multiple blanks into single ones
     data_after = ' '.join(data_clean.split())
 
-    #print file, '\n', data_clean, '\n'
+    #let all the string tart from letter and end with letter
+    idx_head = re.search(r_head, data_after)
+    #print file, '\n', data_after[idx_head.start() :], '\n'
+
+
+    '''
+    idx_gethead = idx_head.start()
+    data_after = data_after[idx_gethead:]
     
-    Text = Text + data
+    print file, '\n', data_after, '\n'
+    data_after = data_after.strip()
+
+    '''
+
+    #replace all the ...
+    data_after = data_after.replace('...', '')
+    #print file, '\n', data_after, '\n'
+
+    #start to set boundary
+    boundList = [' ? ', ' ! ', ' . ']
+    for i in boundList:
+
+        data_after = data_after.replace(i,' <s> ' )
+
+
+    #get rid of small residual in the email
+    if data_after.rfind(' <s> ') != -1 and len(data_after[data_after.rfind(' <s> '):]) <30:
+        data_after = data_after[0:data_after.rfind(' <s> ')]
+
+    #set boundary in the end of each string
+    data_after = data_after.rstrip(".!?")
+
+    data_fin = data_after + ' <s> ' 
+
+    #get a string for all the email in the folder
+    Text = Text + data_fin
+    Text = ' '.join(Text.split())
+
+print Text
 
 
 # create word types and their frequencies
@@ -125,22 +167,24 @@ biGramFreq = pd.DataFrame( [0]*(len(vocabulary) * len(vocabulary) ), index = voc
 
 
 # create a n*n matrix to store prob for all n words
+TextList = Text.split(' ')
+TextListLen = len(TextList)
+TextList = TextList[:(TextListLen/2)]
 
-wd_base = list(set(Text.split(' ')))
+
+wd_base = list(set(TextList))
 print len(wd_base)
 
-wd_freq = collections.Counter(Text.split(' '))
-#print wd_freq
+wd_freq = collections.Counter(TextList)
+print wd_freq
 
-'''
+
 mat = {}
 for wd in wd_base[:100]:
+    wdFreq = max(TextList.count(wd),1)
     mat[wd] = {}
-    wdFreq = max(Text.split(' ').count(wd),1)
-    for wd1 in wd_base[:100]:
-        biGram = wd + ' ' + wd1
-        mat[wd][wd1] = 1.* Text.count(biGram) / wdFreq
-'''
+    for wd1 in wd_base:
+        mat[wd][wd1] = 1.* TextList.count(wd + ' ' + wd1) / wdFreq
 
 
 
